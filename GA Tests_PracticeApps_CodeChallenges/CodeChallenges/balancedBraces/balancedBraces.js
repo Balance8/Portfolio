8c93a5229ca37038098c2a765aae2c2a4549f58a
Michael Slocum <mslocum819@gmail.com>
2018-07-07 19:47:14 -0700

    CodeChallenge

--- GA Tests_PracticeApps_CodeChallenges/CodeChallenges/balancedBraces/balancedBraces.js
+++ GA Tests_PracticeApps_CodeChallenges/CodeChallenges/balancedBraces/balancedBraces.js
@@ -1,13 +1,32 @@
 function balancedBraces(str) {
-  stack = [];
-  str.split("").forEach(x => {
-    x === "{" || x === "[" || x === "(" ? stack.push(x) : x === "}" || x === "]" || x === ")" ? stack.pop(x) : "";
+  let stack = [];
+  let map = { "}": "{", ")": "(", "]": "[" };
 
-  });
-  console.log(stack);
-  return stack.length === 0;
+  
+
+  for (let index = 0; index < str.length; index++) {
+    let curr = str[index];
+
+    if (["(", "{", "["].includes(curr)) {
+      stack.push(curr);
+    } else {
+      let top = stack.pop();
+      if (top !== map[curr]) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
-console.log(balancedBraces("{xx[x()x]xx}"));
+console.log(balancedBraces("{xx[x()x]xxx}"));
 
-console.log(balancedBraces("{xx[x)x]xx}"));
+console.log(balancedBraces("{xx[{((((x]xx}"));
+
+console.log(
+  balancedBraces("(){}[]"), // => true
+  balancedBraces("([{}])"), // => true
+  balancedBraces("(}"), // => false
+  balancedBraces("[(])"), // => false
+  balancedBraces("[({})](]") // => false
+);
