5beb5f3a0b0f0df4778da37e5486ccc67d847397
Michael Slocum <mslocum819@gmail.com>
2018-07-10 15:22:06 -0700

    Tests

--- GA Tests_PracticeApps_CodeChallenges/CodeChallenges/balancedBraces/balancedBraces.js
+++ GA Tests_PracticeApps_CodeChallenges/CodeChallenges/balancedBraces/balancedBraces.js
@@ -1,35 +1,28 @@
 function balancedBraces(str) {
   let stack = [];
   let map = { "}": "{", ")": "(", "]": "[" };
-  let bValue;
 
   // console.log(Object.keys(map));
   // console.log(Object.values(map));
-  console.log(JSON.parse(JSON.stringify(map["}"])));
-
+  // console.log(JSON.parse(JSON.stringify(map["}"])));
   str.split("").forEach(x => {
-    console.log(x);
-
     ["(", "{", "["].includes(x)
       ? stack.push(x)
       : stack[stack.length - 1] === map[x]
         ? stack.pop()
         : "";
   });
-
-  console.log(stack);
-
   return stack.length === 0;
 }
 
-console.log(balancedBraces("{[(xx)]}"));
+console.log(balancedBraces("{[(x)x]asd}"));
 
-// console.log(balancedBraces("{x[{((((]}"));
+console.log(balancedBraces("{x[{((((]}"));
 
-// console.log(
-//   balancedBraces("(){}[]"), // => true
-//   balancedBraces("([{}])"), // => true
-//   balancedBraces("(}"), // => false
-//   balancedBraces("[(])"), // => false
-//   balancedBraces("[({})](]") // => false
-// );
+console.log(
+  balancedBraces("(){}[]"), // => true
+  balancedBraces("([{}])"), // => true
+  balancedBraces("(}"), // => false
+  balancedBraces("[(])"), // => false
+  balancedBraces("[({})](]") // => false
+);
