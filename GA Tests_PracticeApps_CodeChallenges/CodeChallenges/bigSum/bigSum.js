8768f5173beb3c206f1369bab0ead4d196fea754
Michael Slocum <mslocum819@gmail.com>
2018-07-10 17:48:11 -0700

    bigSum

---
+++ GA Tests_PracticeApps_CodeChallenges/CodeChallenges/bigSum/bigSum.js
@@ -0,0 +1,11 @@
+function aVeryBigSum(ar) {
+  let sum = 0;
+  ar.forEach(e => {
+    sum += e;
+  });
+  return sum;
+}
+
+console.log(
+  aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005])
+);
