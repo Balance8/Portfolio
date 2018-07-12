eb061f13d33f50b2359207baf7a92f39a8f454f2
Michael Slocum <mslocum819@gmail.com>
2018-07-12 15:07:26 -0700

    cc

--- GA Tests_PracticeApps_CodeChallenges/CodeChallenges/diagonalDifference/diagonalDifference.js
+++ GA Tests_PracticeApps_CodeChallenges/CodeChallenges/diagonalDifference/diagonalDifference.js
@@ -1,20 +1,20 @@
-function diagnalDifference(n, arr) {
+function diagonalDifference(arr) {
   let d1 = 0;
   let d2 = 0;
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      console.log(i + " i");
-      console.log(j + " j");
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      // console.log(i + " i");
+      // console.log(j + " j");
       if (i === j) d1 += arr[i][j];
-      k = n - j - 1;
+      let k = arr.length - j - 1;
       // console.log(k + " k");
       if (i === k) d2 += arr[i][j];
     }
   }
-  console.log(d1 + " d1");
-  console.log(d2 + " d2");
+  // console.log(d1 + " d1");
+  // console.log(d2 + " d2");
   return Math.abs(d1 - d2);
 }
 
-console.log(diagnalDifference(3, [[11, 2, 4], [4, 5, 6], [10, 8, -12]]));
+console.log(diagonalDifference([[11, 2, 4], [4, 5, 6], [10, 8, -12]]));
