4a9eec53e4e6a8c12fb41f902d5a000fec63f564
Michael Slocum <mslocum819@gmail.com>
2018-07-12 23:31:04 -0700

    cc

--- GA Tests_PracticeApps_CodeChallenges/CodeChallenges/miniMaxSum/miniMaxSum.js
+++ GA Tests_PracticeApps_CodeChallenges/CodeChallenges/miniMaxSum/miniMaxSum.js
@@ -2,15 +2,13 @@ let miniMaxSum = arr => {
   let [min, max] = [[], []];
   let sorted = arr.sort((a, b) => a - b);
 
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     min.push(sorted[i]);
   }
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length - 1; i++) {
     max.push(sorted[i]);
   }
-
-  console.log(min);
-  console.log(max);
+  console.log(min.reduce((a, b) => a + b, 0), max.reduce((a, b) => a + b, 0));
 };
 
 console.log(miniMaxSum([1, 20, 2, 3, 4, 5]));
