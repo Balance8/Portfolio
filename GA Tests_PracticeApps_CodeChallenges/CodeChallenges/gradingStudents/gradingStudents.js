4f0e1be520aaf984ec9cc77407594f994fb1b605
Michael Slocum <mslocum819@gmail.com>
2018-07-16 11:44:11 -0700

    cc

--- GA Tests_PracticeApps_CodeChallenges/CodeChallenges/gradingStudents/gradingStudents.js
+++ GA Tests_PracticeApps_CodeChallenges/CodeChallenges/gradingStudents/gradingStudents.js
@@ -1,8 +1,28 @@
 function gradingStudents(grades) {
-  finalScore = 0;
+  let fGrade = [];
   grades.forEach(grade => {
-    grade > 40 ? console.log(grade % 10) : console.log(grade);
+    grade < 38 || grade % 5 < 3
+      ? fGrade.push(grade)
+      : fGrade.push(grade + (5 - (grade % 5)));
   });
+  return fGrade;
 }
+//   grades.forEach(grade => {
+//     let fill = (10 - (grade % 10) + (grade % 10)) / 2 - (grade % 10);
+//     console.log(fill);
+//     grade < 37
+//       ? console.log(grade)
+//       : fill < 3 && fill > 0
+//         ? console.log(`${grade}`[0] + 5)
+//         : console.log(Math.ceil(grade));
 
-gradingStudents([73, 67, 38, 33]);
+// grade > 40
+//   ? grade % 10 < 5
+//     ? (10 - (grade % 10) + (grade % 10)) / 2 - (grade % 10) > 3
+//       ? console.log(`${grade}`[0] + 5)
+//       : console.log(Math.ceil(grade))
+//     : console.log(Math.ceil(grade))
+//   : console.log(grade);
+//   });
+
+console.log(gradingStudents([73, 67, 38, 33]));
