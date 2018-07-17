c63c81b2c634e307211359da59023a8fbae0de8c
Michael Slocum <mslocum819@gmail.com>
2018-07-17 16:32:48 -0700

    cc

--- GA Tests_PracticeApps_CodeChallenges/CodeChallenges/kangaroo/kangaroo.js
+++ GA Tests_PracticeApps_CodeChallenges/CodeChallenges/kangaroo/kangaroo.js
@@ -1,14 +1,30 @@
 function kangaroo(x1, v1, x2, v2) {
-  let ff = { k1: 0, k2: 0 };
-  for (let index = 2; index < 3; index++) {
-    ff.k2 = (x2 + v2) * index;
+  if (v1 < v2) return "NO";
+  if (x1 < x2 && v1 > v2) {
+    const xDiff = Math.abs(x1 - x2);
+    const vDiff = Math.abs(v1 - v2);
+    console.log(xDiff, vDiff);
+    if (xDiff % vDiff == 0) {
+      return "YES";
+    } else {
+      return "NO";
+    }
   }
-  for (let index = 2; index < 3; index++) {
-    ff.k2 = (x2 + v2) * index;
-  }
+  return "NO";
+  //   let ff = { k1: 0, k2: 0 };
+  //   let yn = "NO";
+  //   for (let index = 2; index < 3; index++) {
+  //     ff.k2 = (x2 + v2) * index;
+  //   }
+  //   for (let index = 1; index < 10; index++) {
+  //     ff.k1 === ff.k2 ? (yn = "YES") : (ff.k1 = (x1 + v1) * index);
+  //   }
+
+  //   return yn;
 }
 
 console.log(kangaroo(0, 3, 4, 2));
+console.log(kangaroo(0, 2, 5, 3));
 /* k1 starts at 0 and jumps 3 times 
    k2 starts at 4 and jumps 2 times
 
