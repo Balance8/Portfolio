b37e297ff962672b1e0bbd66a5d54ad468e34da5
Michael Slocum <mslocum819@gmail.com>
2018-07-17 00:47:08 -0700

    cc

--- GA Tests_PracticeApps_CodeChallenges/CodeChallenges/countApplesAndOranges/countApplesAndOranges.js
+++ GA Tests_PracticeApps_CodeChallenges/CodeChallenges/countApplesAndOranges/countApplesAndOranges.js
@@ -1,10 +1,18 @@
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
-    let house = t - s;
-    console.log(house) // 4
-    apples.forEach(apple => {
-        
-    });
-
+  let result = { applesR: 0, orangesR: 0 };
+  let house = t - s;
+  let houseToAppleStart = s - a;
+  let houseToAppleEnd = t - a;
+  console.log(house); // 4
+  console.log(houseToAppleStart); //2
+  console.log(houseToAppleEnd); //6
+  apples.forEach(apple => {
+    console.log(a + apple + " a + apple");
+    a + apple >= houseToAppleStart && a + apple <= houseToAppleEnd && apple >= 0
+      ? result.applesR++
+      : "";
+  });
+  return result;
 }
 
-countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
+console.log(countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]));
