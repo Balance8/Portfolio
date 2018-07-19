40aa7203908ca11d8c2664dc50d9ea1567497855
Michael Slocum <mslocum819@gmail.com>
2018-07-18 23:32:36 -0700

    cc

--- GA Tests_PracticeApps_CodeChallenges/CodeChallenges/breakingRecords/breakingRecords.js
+++ GA Tests_PracticeApps_CodeChallenges/CodeChallenges/breakingRecords/breakingRecords.js
@@ -3,19 +3,15 @@ function breakingRecords(scores) {
   let minMax = { max: 0, min: 0 };
 
   scores.forEach(e => {
-    // console.log(Math.max(...newArray), Math.min(...newArray));
-    // console.log(newArray);
     e > Math.max(...newArray) && newArray.length > 0
       ? minMax.max++
       : e < Math.min(...newArray) && newArray.length > 0
         ? minMax.min++
         : "";
-    console.log(minMax);
     newArray.push(e);
   });
 
-  console.log(newArray);
-  console.log(minMax);
+  return [minMax.max, minMax.min];
 }
 
-breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]);
+console.log(breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]));
